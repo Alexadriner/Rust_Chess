92a50653ffce6d6a51b3be937199abd2dbd3fd60
Alexadriner <alex02.rust@gmail.com>
2025-10-05 19:41:58 +0200

    Little changes

--- src/game_objects/chess_piece.rs
+++ src/game_objects/chess_piece.rs
@@ -4,4 +4,13 @@ pub struct Chess_piece<const N: usize> {
     pub movement: [[u8; 2]; N],
     pub repeating_movement: bool,
     pub only_front_movement: bool,
+    pub position: [u8; 2],
+    pub color: String,
+    pub is_king: bool,
+}
+
+impl<const N: usize> Chess_piece<N> {
+    fn get_move_options (&self) -> Vec<u8> {
+        return vec![0, 1, 2];
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ mod game_objects;
 use game_objects::chess_piece;
 use crate::game_objects::chess_piece::Chess_piece;
 
+mod gui;
+use gui::chess_gui;
+use crate::gui::chess_gui::ui_counter;
+
+use egui;
+
 fn main() {
     
 }
