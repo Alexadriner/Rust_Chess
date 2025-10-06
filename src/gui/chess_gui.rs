183ea185fa80409cd806a377269d369951aff5d1
Alexadriner <alex02.rust@gmail.com>
2025-10-06 17:38:59 +0200

    Add Gui

--- src/gui/chess_gui.rs
+++ src/gui/chess_gui.rs
@@ -1,1 +1,12 @@
-use egui;
+use eframe::egui;
+
+#[derive(Default)]
+pub struct App;
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hii");
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ use crate::game_objects::chess_piece::Chess_piece;
 mod gui;
 use gui::chess_gui;
 
-use egui;
+use eframe::egui;
 
-fn main() {
-    
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Ok(Box::new(chess_gui::App::default()))),
+    )
 }
